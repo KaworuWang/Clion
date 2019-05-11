8116aff45c3937ada94803d09116b6fd609607be
KaworuWang <KaworuWang@users.noreply.github.com>
2019-05-11 22:13:46 +0800

    Update 20190510.c

--- 20190510.c
+++ 20190510.c
@@ -16,9 +16,9 @@ int main()
 {
 int i,j,k;
 printf ("\n");
-for (i = 1; i < 5; i ++ ) { //以下为三重循环
-for (j = 1; j <5; j ++){
-for (k =1; k < 5; k ++){ // 确保i, j, k 三位互不相同
+for (i = 1; i < 5; i ++) { //以下为三重循环
+for (j = 1; j < 5; j ++) {
+for (k = 1; k < 5; k ++) { // 确保i, j, k 三位互不相同
 if (i != k && i != j && j != k) {
 printf("%d,%d,%d\n", i, j, k);
 }
