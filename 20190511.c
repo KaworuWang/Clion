fe8f3114cc9e6f9acf3ef36e1efd8538834acebf
KaworuWang <KaworuWang@users.noreply.github.com>
2019-05-14 16:56:34 +0800

    20190511

---
+++ 20190511.c
@@ -0,0 +1,15 @@
+//
+// Created by KW on 2019/5/14.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+
+    printf("i am %d years old.\n", age);
+    printf("i am %d inches tall.\n", height);
+
+    return 0;
--- main.c
+++ main.c
@@ -1,14 +1,3 @@
-#include <stdio.h>
-
-int main()
-{
-  int age = 10;
-  int height = 72;
-  
-  printf("i am %d years old.\n", age);
-  printf("i am %d inches tall.\n", height);
-  
-  return 0;
 }
 
 //练习6 结构
