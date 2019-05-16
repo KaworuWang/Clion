9c59a62afe01300145ea058954d91512570a17ec
KaworuWang <KaworuWang@users.noreply.github.com>
2019-05-16 14:49:49 +0800

    20190516

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 //练习6 结构
 
-#includ <stdio.h>
+#include <stdio.h>
 
 int main(int argc,  char *argv[])
 {
   int distance = 100;
   
   printf("Your ard %d miles away.\n", distance);
-  
+  printf("YY");
   return 0;
 }
 
