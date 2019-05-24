9a0304180c9a7da90418cf088182a5c361715480
KaworuWang <KaworuWang@users.noreply.github.com>
2019-05-24 14:00:36 +0800

    20190524

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
-这段代码中我们创建了一些不同数据类型的数组。由于数组是C语言工作机制的核心，有大量的方法可以用来创建数组。我们暂且使用type name[] = {initializer};语法，之后我们会深入研究。这个语法的意思是，“我想要那个类型的数组并且初始化为{..}”。C语言看到它时，会做这些事情：
-查看它的类型，以第一个数组为例，它是int。
-查看[]，看到了没有提供长度。
-查看初始化表达式{10, 12, 13, 14, 20}，并且了解你想在数组中存放这5个整数。
-在电脑中开辟出一块空间，可以依次存放这5个整数。
-将数组命名为areas，也就是你想要的名字，并且在当前位置给元素赋值。
+#include <stdio.h>
 
-在这段代码中，我们创建了一些数组，并对数组元素赋值。在numbers中我们设置了一些数字，然而在names中我们实际上手动构造了一个字符串。
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    // go through each string in argv
+    for(i = 1; i < argc; i++){
+        printf("arg %d %s\n", i, argv[i]);
+    }
+
+    char *states[] = {
+            "Californai", "Oregon", "Washington", "Texas"
+    };
+    int num_states = 4;
+
+    for(i = 0; i < num_states; i++){
+        printf("state %d: %s\n", i, states[i]);
+    }
+
+    return 0;
+}
