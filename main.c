7a7ddc231521c1291276f9a3ca947fcacf41575e
KaworuWang <KaworuWang@users.noreply.github.com>
2019-05-24 14:06:32 +0800

    20190524

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+/*
+ * #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +17,34 @@ int main(int argc, char *argv[])
 
     for(i = 0; i < num_states; i++){
         printf("state %d: %s\n", i, states[i]);
+    }
+
+    return 0;
+}
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    //go through each string in argv
+
+    int i = 0;
+    while(i < argc) {
+        printf("arg %d: %s\n", i, argv[i]);
+        i++;
+    }
+
+    //make strings
+    char *states[] = {
+            "California", "Oregon", "Washington", "Texas"
+    };
+
+    int num_states = 4;
+    i = 0;
+    while (i < num_states) {
+        printf("states %d %s\n", i, states[i]);
+        i++;
     }
 
     return 0;
