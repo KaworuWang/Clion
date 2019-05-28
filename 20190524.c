7948f2f0a274322b1599b4ad8598ffe6133177c8
KaworuWang <KaworuWang@users.noreply.github.com>
2019-05-28 19:13:31 +0800

    20190528

--- 20190524.c
+++ 20190524.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
 
-// forward declarations
+//forward declarations
 int can_print_it(char ch);
-void print_letters(const char arg[]);
+void print_letters(char arg[]);
 
 void print_arguments(int argc, char *argv[])
 {
     int i = 0;
 
-    for(i = 0; i < argc; i++) {
+    for (i = 0; i < argc; i++){
         print_letters(argv[i]);
     }
 }
@@ -18,11 +18,11 @@ void print_letters(const char arg[])
 {
     int i = 0;
 
-    for(i = 0; arg[i] != '\0'; i++) {
+    for(i = 0; arg[i] != '\0'; i++){
         char ch = arg[i];
 
-        if(can_print_it(ch)) {
-            printf("'%c' == %d ", ch, ch);
+        if(can_print_it(ch)){
+            printf("'%c' == %d", ch, ch);
         }
     }
 
@@ -33,7 +33,6 @@ int can_print_it(char ch)
 {
     return isalpha(ch) || isblank(ch);
 }
-
 
 int main(int argc, char *argv[])
 {
