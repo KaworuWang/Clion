8bae647d11b49b9e1afa77d877acdfb42c9cdd2e
KaworuW <KaworuWang@users.noreply.github.com>
2019-07-03 22:55:23 +0800

    Update 20190613.c

--- 20190613.c
+++ 20190613.c
@@ -32,4 +32,8 @@ main()
     /*再加上某天的天数*/ 
     
     if(year%400==0||(year%4==0&&year%100!=0)) 
-        /*判断是不是闰年*/ leap=1; else leap=0; if(leap==1&&month>2) /*如果是闰年且月份大于2,总天数应该加一天*/ sum++; printf("It is the %dth day.",sum); }
+        /*判断是不是闰年*/ 
+        leap=1; 
+    else leap=0; 
+    if(leap==1&&month>2) 
+        /*如果是闰年且月份大于2,总天数应该加一天*/ sum++; printf("It is the %dth day.",sum); }
