6be329bed67a38d2d61c35f94351853693d86f03
KaworuW <KaworuWang@users.noreply.github.com>
2019-07-06 17:34:23 +0800

    Update 20190613.c

--- 20190613.c
+++ 20190613.c
@@ -23,8 +23,10 @@ main()
             break; 
         case 7:sum=181;
             break; 
-        case 8:sum=212;break; 
-        case 9:sum=243;break; 
+        case 8:sum=212;
+            break; 
+        case 9:sum=243;
+            break; 
         case 10:sum=273;break; 
         case 11:sum=304;break;
         case 12:sum=334;break;
