27ed2739d3309020aa57273ff397effe7927c867
KaworuW <KaworuWang@users.noreply.github.com>
2019-07-20 10:52:12 +0800

    20190720

--- 20190613.c
+++ 20190613.c
@@ -1,51 +1,51 @@
-s#include <stdio.h>
+#include <stdio.h>
 
 main()
 {
     int day,month,year,sum,leap;
     printf("\nplease input year,month,day\n");
     scanf("%d,%d,%d",&year,&month,&day);
-    switch(month) 
+    switch(month)
         /*先计算某月以前月份的总天数*/
     {
         case 1:sum=0;
-            break; 
+            break;
         case 2:sum=31;
             break;
         case 3:sum=59;
-            break; 
+            break;
         case 4:sum=90;
-            break; 
+            break;
         case 5:sum=120;
-            break; 
+            break;
         case 6:sum=151;
-            break; 
+            break;
         case 7:sum=181;
-            break; 
+            break;
         case 8:sum=212;
-            break; 
+            break;
         case 9:sum=243;
-            break; 
+            break;
         case 10:sum=273;
-            break; 
+            break;
         case 11:sum=304;
             break;
         case 12:sum=334;
             break;
         default:printf("data error");
-            break; 
-    } 
-    
-    sum=sum+day; 
-    /*再加上某天的天数*/ 
-    
-    if(year%400==0||(year%4==0&&year%100!=0)) 
-        /*判断是不是闰年*/ 
-        leap=1; 
-    else 
-        leap=0; 
-    if(leap==1&&month>2) 
-        /*如果是闰年且月份大于2,总天数应该加一天*/ 
-        sum++; 
-    printf("It is the %dth day.",sum); 
+            break;
+    }
+
+    sum = sum + day;
+    /*再加上某天的天数*/
+
+    if(year%400==0||(year%4==0&&year%100!=0))
+        /*判断是不是闰年*/
+        leap=1;
+    else
+        leap=0;
+    if(leap==1&&month>2)
+        /*如果是闰年且月份大于2,总天数应该加一天*/
+        sum++;
+    printf("It is the %dth day.",sum);
 }
