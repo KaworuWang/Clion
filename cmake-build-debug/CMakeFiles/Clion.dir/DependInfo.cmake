c66b9cdb1f8d9ee50920ad17d0bc170b3146698d
KaworuW <KaworuWang@users.noreply.github.com>
2019-07-20 10:05:28 +0800

    20190720

--- cmake-build-debug/CMakeFiles/Clion.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Clion.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/CM_ka/CLionProjects/Clion/main.c" "C:/Users/CM_ka/CLionProjects/Clion/cmake-build-debug/CMakeFiles/Clion.dir/main.c.obj"
+  "D:/GitHub/Clion/main.c" "D:/GitHub/Clion/cmake-build-debug/CMakeFiles/Clion.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
